00462280b321225ef7c171c1950bd1f1ce2947bd
Sukhdeep <Sukhdeep@LAPTOP-86RC0SIN>
2017-08-18 10:01:34 +1200

    change in display

--- A3/src/timetable1/course_2.java
+++ A3/src/timetable1/course_2.java
@@ -6,8 +6,15 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import com.mysql.jdbc.PreparedStatement;
+
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.awt.Color;
 
 public class course_2 extends JFrame {
@@ -30,10 +37,14 @@ public class course_2 extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public course_2() {
+
+    /**
+       * Create the frame.
+        * @throws SQLException 
+         */
+        public course_2() throws SQLException {
+
+	
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
@@ -42,22 +53,22 @@ public class course_2 extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Student ID");
+		JLabel lblNewLabel = new JLabel("STUDENTID");
 		lblNewLabel.setFont(new Font("Monospaced", Font.PLAIN, 13));
 		lblNewLabel.setBounds(50, 83, 93, 14);
 		contentPane.add(lblNewLabel);
 		
-		JLabel lblNewLabel_1 = new JLabel("Day");
+		JLabel lblNewLabel_1 = new JLabel("INSTRUCTOR");
 		lblNewLabel_1.setFont(new Font("Monospaced", Font.PLAIN, 13));
 		lblNewLabel_1.setBounds(50, 160, 83, 14);
 		contentPane.add(lblNewLabel_1);
 		
-		JLabel lblNewLabel_2 = new JLabel("Time");
+		JLabel lblNewLabel_2 = new JLabel("DAY");
 		lblNewLabel_2.setFont(new Font("Monospaced", Font.PLAIN, 13));
 		lblNewLabel_2.setBounds(50, 200, 83, 14);
 		contentPane.add(lblNewLabel_2);
 		
-		JLabel lblNewLabel_3 = new JLabel("Instructor");
+		JLabel lblNewLabel_3 = new JLabel("TIME");
 		lblNewLabel_3.setFont(new Font("Monospaced", Font.PLAIN, 13));
 		lblNewLabel_3.setBounds(50, 120, 83, 14);
 		contentPane.add(lblNewLabel_3);
@@ -66,6 +77,86 @@ public class course_2 extends JFrame {
 		lblNewLabel_4.setFont(new Font("Monospaced", Font.PLAIN, 16));
 		lblNewLabel_4.setBounds(139, 31, 102, 14);
 		contentPane.add(lblNewLabel_4);
-	}
+		
+		
+	
+        JLabel stdIdlbl = new JLabel("");
+        stdIdlbl.setBounds(172, 85, 115, 15);
+        contentPane.add(stdIdlbl);
+
+       JLabel instructorlbl = new JLabel("");
+       instructorlbl.setBounds(182, 120, 115, 15);
+       contentPane.add(instructorlbl);
+
+       JLabel daylbl = new JLabel("");
+       daylbl.setBounds(172, 165, 115, 15);
+       contentPane.add(daylbl);
+
+       JLabel timelbl = new JLabel("");
+       timelbl.setBounds(172, 206, 115, 15);
+       contentPane.add(timelbl);
+
+      //*******************************
+     ResultSet myresultset=null;
+      PreparedStatement prepstmt;
+      Connection myDBconnection =null;
+       int studentID;
+       String name2;
+
+
+     myDBconnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/s1","root","");
+
+    System.out.println("Connected to DB");
+	    
+    String sqlQuery=("SELECT * FROM s1.course2"); // ensure that your schema name is correct.
+
+   prepstmt = (PreparedStatement) myDBconnection.prepareStatement(sqlQuery);
+ 					 		 
+   myresultset= prepstmt.executeQuery();
+										 
+   while(myresultset.next()){
+																							 
+	  if(myresultset.getString("StudentID").equals("2")){
+		
+		studentID=myresultset.getInt("STUDENTID");
+		stdIdlbl.setText(String.valueOf(studentID));
+					
+												 				 				 				 			 
+   }
+	
+    if(myresultset.getString("instructor").equals("MESA")){
+		
+        name2=myresultset.getString("Instructor");
+        instructorlbl.setText(String.valueOf(name2));
+	
+	
+ }
+
+    if(myresultset.getString("Day").equals("TUESDAY")){
+
+     name2=myresultset.getString("Day");
+     daylbl.setText(String.valueOf(name2));
+
 
 }
+
+   if(myresultset.getString("Time").equals("14:00")){
+
+   name2=myresultset.getString("Time");
+   timelbl.setText(String.valueOf(name2));
+
+
+}
+
+
+		
+
+}
+
+}}
+
+ 
+
+
+	
+
